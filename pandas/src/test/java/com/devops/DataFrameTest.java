53d47e6c43a3260766ce6e4a2a05a1e77f550ff2
Daemianas <lombar2d@laposte.net>
2022-04-19 15:13:21 +0200

    test
    Co-authored-by: Dorian-Gray73 <Dorian-Gray73@users.noreply.github.com>

--- pandas/src/test/java/com/devops/DataFrameTest.java
+++ pandas/src/test/java/com/devops/DataFrameTest.java
@@ -7,6 +7,22 @@ import java.util.ArrayList;
 import org.junit.Test;
 
 public class DataFrameTest {
+
+    public <E> ArrayList<E> createArrayList(Integer[] tab){
+        ArrayList<E> list = new ArrayList<E>();
+        for (Integer i : tab) {
+            list.add((E) i);
+        }
+        return list;
+    }
+
+    public <E> ArrayList<E> createArrayList(String[] tab){
+        ArrayList<E> list = new ArrayList<E>();
+        for (String i : tab) {
+            list.add((E) i);
+        }
+        return list;
+    }
     
     @Test
     public void CreateDataFrameFromData(){
@@ -42,7 +58,7 @@ public class DataFrameTest {
             e.printStackTrace();
         }
     }
-
+/*
     @Test
     public void CreateDataFrameFromCSV(){
         try {
@@ -61,6 +77,233 @@ public class DataFrameTest {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }//*/
+
+    @Test
+    public void TestnbLigne(){
+        ArrayList<String> labels = new ArrayList<String>();
+            labels.add("col1");
+            labels.add("col2");
+            labels.add("col3");
+            ArrayList<ArrayList<?>> data = new ArrayList<ArrayList<?>>();
+            ArrayList<Integer> colonne1 = new ArrayList<Integer>();
+            colonne1.add(1);
+            colonne1.add(2);
+            colonne1.add(3);
+            data.add(colonne1);
+        DataFrame df;
+        try {
+            df = new DataFrame(labels, data);
+            assertEquals("Nombre de ligne : ",3, df.nbLigne());
+        } catch (Exception e) {
+            
+            e.printStackTrace();
+        }
+    }
+    
+    @Test
+    public void TestnbLigneError(){
+        ArrayList<String> labels = new ArrayList<String>();
+            labels.add("col1");
+            labels.add("col2");
+            labels.add("col3");
+            ArrayList<ArrayList<?>> data = new ArrayList<ArrayList<?>>();
+        DataFrame df;
+        try {
+            df = new DataFrame(labels, data);
+            assertEquals("Nombre de colonne ",0,df.nbLigne());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
+    @Test
+    public void TestnbColonne(){
+        ArrayList<String> labels = new ArrayList<String>();
+            labels.add("col1");
+            labels.add("col2");
+            labels.add("col3");
+            ArrayList<ArrayList<?>> data = new ArrayList<ArrayList<?>>();
+            ArrayList<Integer> colonne1 = new ArrayList<Integer>();
+            colonne1.add(1);
+            colonne1.add(2);
+            colonne1.add(3);
+            data.add(colonne1);
+        DataFrame df;
+        try {
+            df = new DataFrame(labels, data);
+            assertEquals("Nombre de colonne : ",3, df.nbColonne());
+        } catch (Exception e) {
+            
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void TestnbColonneError(){
+        ArrayList<String> labels = new ArrayList<String>();
+            ArrayList<ArrayList<?>> data = new ArrayList<ArrayList<?>>();
+        DataFrame df;
+        try {
+            df = new DataFrame(labels, data);
+            assertEquals("Nombre de colonne ",0,df.nbColonne());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void TestGetColumn(){
+        ArrayList<String> labels = new ArrayList<String>();
+            labels.add("col1");
+            labels.add("col2");
+            labels.add("col3");
+            ArrayList<ArrayList<?>> data = new ArrayList<ArrayList<?>>();
+            ArrayList<Integer> colonne1 = new ArrayList<Integer>();
+            colonne1.add(1);
+            colonne1.add(2);
+            colonne1.add(3);
+            data.add(colonne1);
+            ArrayList<Integer> colonne2 = new ArrayList<Integer>();
+            colonne2.add(4);
+            colonne2.add(5);
+            colonne2.add(6);
+            data.add(colonne2);
+            ArrayList<Integer> colonne3 = new ArrayList<Integer>();
+            colonne3.add(7);
+            colonne3.add(8);
+            colonne3.add(9);
+            data.add(colonne3);
+        DataFrame df;
+        try {
+            df = new DataFrame(labels, data);
+            assertEquals("Nombre de colonne : ",3, df.nbColonne());
+            assertEquals("Obtenir la premiere colonne", colonne1, df.getColumn("col1"));
+            assertEquals("Obtenir la deuxieme colonne", colonne2, df.getColumn("col2"));
+            assertEquals("Obtenir la troisieme colonne", colonne3, df.getColumn("col3"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //TODO : Test getColumnError
+    /*
+    @Test (expected = IllegalArgumentException.class)
+    public void TestGetColumnError() {
+        ArrayList<String> labels = new ArrayList<String>();
+            labels.add("col1");
+            labels.add("col2");
+            labels.add("col3");
+            ArrayList<ArrayList<?>> data = new ArrayList<ArrayList<?>>();
+            ArrayList<Integer> colonne1 = new ArrayList<Integer>();
+            colonne1.add(1);
+            colonne1.add(2);
+            colonne1.add(3);
+            data.add(colonne1);
+        DataFrame df;
+        try {
+            df = new DataFrame(labels, data);
+            df.getColumn("col4");
+        } catch (IllegalArgumentException e) {
+            throw e;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+    }
+    */
+ 
+    @Test
+    public void TestHead_int(){
+        ArrayList<String> labels = new ArrayList<String>();
+            labels.add("col1");
+            labels.add("col2");
+            labels.add("col3");
+            ArrayList<ArrayList<?>> data = new ArrayList<ArrayList<?>>();
+            ArrayList<Integer> colonne1 = new ArrayList<Integer>();
+            colonne1.add(1);
+            colonne1.add(2);
+            colonne1.add(3);
+            data.add(colonne1);
+            ArrayList<Integer> colonne2 = new ArrayList<Integer>();
+            colonne2.add(4);
+            colonne2.add(5);
+            colonne2.add(6);
+            data.add(colonne2);
+            ArrayList<Integer> colonne3 = new ArrayList<Integer>();
+            colonne3.add(7);
+            colonne3.add(8);
+            colonne3.add(9);
+            data.add(colonne3);
+        DataFrame df;
+        try {
+            df = new DataFrame(labels, data);
+            assertEquals("Nombre de colonne : ",3, df.nbColonne());
+            assertEquals("Obtenir la premiere colonne","col1\tcol2\tcol3\t\n1\t4\t7\t\n", df.head(1));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+     @Test
+    public void TestHead(){
+        ArrayList<String> labels = new ArrayList<String>();
+            labels.add("col1");
+            labels.add("col2");
+            labels.add("col3");
+            ArrayList<ArrayList<?>> data = new ArrayList<ArrayList<?>>();
+            ArrayList<Integer> colonne1 = new ArrayList<Integer>();
+            colonne1.add(1);
+            colonne1.add(2);
+            colonne1.add(3);
+            data.add(colonne1);
+            ArrayList<Integer> colonne2 = new ArrayList<Integer>();
+            colonne2.add(4);
+            colonne2.add(5);
+            colonne2.add(6);
+            data.add(colonne2);
+            ArrayList<Integer> colonne3 = new ArrayList<Integer>();
+            colonne3.add(7);
+            colonne3.add(8);
+            colonne3.add(9);
+            data.add(colonne3);
+        DataFrame df;
+        try {
+            df = new DataFrame(labels, data);
+            assertEquals("Nombre de colonne : ",3, df.nbColonne());
+            assertEquals("Obtenir la premiere colonne","col1\tcol2\tcol3\t\n1\t4\t7\t\n2\t5\t8\t\n3\t6\t9\t\n", df.head());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    @Test
+    public void TestToString(){
+        ArrayList<String> labels = new ArrayList<String>();
+            labels.add("col1");
+            labels.add("col2");
+            labels.add("col3");
+            ArrayList<ArrayList<?>> data = new ArrayList<ArrayList<?>>();
+            ArrayList<Integer> colonne1 = new ArrayList<Integer>();
+            colonne1.add(1);
+            colonne1.add(2);
+            colonne1.add(3);
+            data.add(colonne1);
+            ArrayList<Integer> colonne2 = new ArrayList<Integer>();
+            colonne2.add(4);
+            colonne2.add(5);
+            colonne2.add(6);
+            data.add(colonne2);
+        DataFrame df;
+        try {
+            df = new DataFrame(labels, data);
+            assertEquals("Nombre de colonne : ",3, df.nbColonne());
+            assertEquals("Obtenir la premiere colonne","col1\tcol2\tcol3\t\n1\t4\t7\t\n2\t5\t8\t\n", df.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
+
+    
+    
